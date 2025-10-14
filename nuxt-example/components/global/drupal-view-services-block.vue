9f16542cde74ccbe5b336bd520b33378495afba7
Wolfgang Ziegler // fago <fago@wolfgangziegler.net>
2025-10-14 23:20:57 +0200

    style views block with grid

---
+++ nuxt-example/components/global/drupal-view-services-block.vue
@@ -0,0 +1,19 @@
+<template>
+  <HumanifyGrid>
+    <template #content>
+      <component
+        :is="useDrupalCe().renderCustomElements(row)"
+        v-for="(row, index) in rows"
+        :key="index"
+      />
+    </template>
+  </HumanifyGrid>
+</template>
+
+<script setup lang="ts">
+defineProps<{
+  title: string
+  rows?: object[]
+  pager?: object
+}>()
+</script>
